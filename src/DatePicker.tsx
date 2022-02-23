e79abd3bafc1c02e8caa66c6dd98f8977ffeec62
Jonathan Diehl <jonathan.diehl@gmail.com>
2022-02-23 14:56:47 +0100

    prevent disabled days from being selected

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -1,5 +1,6 @@
 import { ReactElement, createElement } from "react";
 import DayPicker, { Modifier } from "react-day-picker";
+import { isSameDay } from "date-fns";
 
 import { DatePickerContainerProps } from "../typings/DatePickerProps";
 
@@ -8,9 +9,6 @@ import "./ui/DatePicker.css";
 
 export function DatePicker(props: DatePickerContainerProps): ReactElement {
     const { value, disableWeekends, disabledDays, disabledDaysKey } = props;
-
-    // change handler
-    const onChange = (d: Date): void => value.setValue(d);
 
     // disabled days
     const disabled: Modifier[] = [];
@@ -23,6 +21,24 @@ export function DatePicker(props: DatePickerContainerProps): ReactElement {
             disabled.push(date);
         }
     }
+
+    // change handler
+    const onChange = (date: Date): void => {
+        // do not allow disbaled days
+        if (disabled.filter(d => d instanceof Date).find(d => isSameDay(d as Date, date))) {
+            return;
+        }
+
+        // do not allow weekends
+        if (disableWeekends) {
+            const weekDay = date.getDay();
+            if (weekDay === 6 || weekDay === 0) {
+                return;
+            }
+        }
+
+        value.setValue(date);
+    };
 
     return <DayPicker selectedDays={value.value} onDayClick={onChange} disabledDays={disabled} />;
 }
